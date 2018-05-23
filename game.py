7975d49475ce4218d04ae9997d5a9b97e162ef6a
James Lambton <lambtonjames@gmail.com>
2018-05-23 17:09:14 +0100

    refactored betting functions, more work required to get all options working

--- game.py
+++ game.py
@@ -41,7 +41,7 @@ class Player(object):
         self.hand = hand
         self.stack = stack
         self.status = "in"
-        self.player_bet = -1
+        self.player_bet = 0.0
 
     def show_hand(self):
         print("{} has hand:".format(self.name))
@@ -70,12 +70,15 @@ class Player(object):
 
         if  self.stack < to_call:
             self.player_bet = self.player_bet + self.stack
+            called_amount = self.stack
             self.stack = 0.0
+            self.status = 'all in'
         else:
             self.player_bet = current_bet
             self.stack = self.stack - to_call
+            called_amount = to_call
 
-        return to_call
+        return called_amount
 
     def raise_bet(self, current_bet):
 
@@ -152,13 +155,11 @@ class Dealer(object):
         for card in street['cards']:
             card.show()
 
-    def receive_bet(self, player, hand):
-        if hand.max_bet == 0:
+    def receive_bet(self, player, round):
+        if round.max_bet == 0:
             bet_received =  player.option_agg()
         else:
-            bet_received = player.option_def(hand.max_bet)
-
-        hand.pot = hand.pot + bet_received
+            bet_received = player.option_def(round.max_bet)
 
         if player.stack == 0:
             player.status = 'all in'
@@ -170,23 +171,46 @@ class Dealer(object):
 
 
 
-class Hand(object):
+class Round(object):
 
     def __init__(self, player_info):
         self.num_active_players = len(player_info)
         self.active_players = []
-        self.all_in_players = []
         self.inactive_players = []
         self.pot = 0
+        self.max_bet = 0
 
     def show_stacks(self, active_players):
         for player in active_players:
             print("{} has stack {}bb".format(player.name, player.stack))
 
     def player_action(self, dealer):
-        pass
+
+        for player in self.active_players:
+
+            if player.status == 'in' and (player.player_bet == 0 or player.player_bet < self.max_bet):
+
+                print("Pot is {}bb".format(self.pot))
+                print("{}, your option, you have {}bb:".format(player.name, player.stack))
+
+                bet_added_to_pot = dealer.receive_bet(player, round)
+                self.pot = self.pot + bet_added_to_pot
+                if player.player_bet >= self.max_bet:
+                    self.max_bet = player.player_bet
+
+                print("{} bet {}bb".format(player.name, bet_added_to_pot))
+                print("{} now has stack {}bb".format(player.name, player.stack))
+
+        for p in range(len(self.active_players) - 1, -1, -1):
+            if self.active_players[p].status == 'out':
+                self.inactive_players.append(self.active_players.pop(p))
+                self.num_active_players -= 1
+            elif self.active_players[p].status == 'all in':
+                self.num_active_players -= 1
 
     def play_street(self, dealer, street):
+
+        self.max_bet = 0
 
         for player in self.active_players:
             player.player_bet = 0
@@ -197,14 +221,16 @@ class Hand(object):
         else:
             dealer.show_cards(street)
 
-        self.show_stacks(self.active_players)
-
         if street['name'] != 'Showdown':
+            self.show_stacks(self.active_players)
             self.player_action(dealer)
-            while not all(b == self.active_players[0] for b in self.active_players):
+
+# need to allow another loop if there is one player left not all in and has the option to call
+
+            while ((not all(player.player_bet == self.active_players[0].player_bet for player in self.active_players))
+                    and self.num_active_players > 1):
+                print("looping")
                 self.player_action(dealer)
-        else:
-            self.show_stacks(self.inactive_players)
 
 
     def play(self):
@@ -223,13 +249,18 @@ class Hand(object):
             if self.num_active_players > 1:
                 self.play_street(dealer, street)
 
+        print("Stacks at end of round:")
+        self.show_stacks(self.active_players)
+        self.show_stacks(self.inactive_players)
+        print("Pot size: {}".format(self.pot))
 
 
-player1 = {'name': 'James', 'stack': 120}
-player2 = {'name': 'Ben', 'stack': 100}
-player3 = {'name': 'Josh', 'stack': 80}
 
-player_info = [player1, player2, player3]
+player1 = {'name': 'Player 1', 'stack': 100}
+player2 = {'name': 'Player 2', 'stack': 100}
+player3 = {'name': 'Player 3', 'stack': 100}
 
-hand = Hand(player_info)
-hand.play()
+player_info = [player1, player2]
+
+round = Round(player_info)
+round.play()
